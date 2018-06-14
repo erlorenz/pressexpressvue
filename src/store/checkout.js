623da3902b37d21930aef99b4bfd4ece93e8633b
Erik Lorenz <erikslorenz@gmail.com>
2018-06-14 16:09:19 -0700

    its working added checkout statuses

--- src/store/checkout.js
+++ src/store/checkout.js
@@ -5,7 +5,9 @@ import { createToken } from 'vue-stripe-elements-plus';
 /* eslint no-param-reassign: ["error",
 { "props": true, "ignorePropertyModificationsFor": ["state"] }] */
 
-const state = {};
+const state = {
+  status: null,
+};
 
 // Getters
 
@@ -16,21 +18,38 @@ const getters = {};
 const actions = {
   checkout: async ({ commit }, payload) => {
     const allCheckoutData = payload;
+
+    commit('CHECKOUT_PENDING');
+
     try {
       const data = await createToken();
       console.log(data.token);
       allCheckoutData.stripeToken = data.token.id;
       const response = await axios.post('http://localhost:3001/checkout', allCheckoutData);
       console.log(response.data);
+      commit('CHECKOUT_SUCCESS');
     } catch (e) {
       console.log(e.message);
+      commit('CHECKOUT_ERROR');
     }
   },
 };
 
 // Mutations
 
-const mutations = {};
+const mutations = {
+  CHECKOUT_SUCCESS: (state) => {
+    state.status = 'success';
+  },
+
+  CHECKOUT_ERROR: (state) => {
+    state.status = 'error';
+  },
+
+  CHECKOUT_PENDING: (state) => {
+    state.status = 'pending';
+  },
+};
 
 export default {
   state,
